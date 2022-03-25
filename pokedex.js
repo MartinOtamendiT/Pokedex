f0d135163a2dffe570f540e29037ac3ca4cc10ca
Martin Otamendi <89677633+MartinOtamendiT@users.noreply.github.com>
2022-03-25 03:59:10 -0600

    Add voice JS

--- pokedex.js
+++ pokedex.js
@@ -1,27 +1,74 @@
-const fetchPokemon = () => {
+async function getPokemon(){
+    //Captura el nombre del pokemon de la entrada.
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
+    //Obtiene archivo del pokemon.
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    fetch(url).then((res) => {
-        if (res.status != "200") {
-            console.log(res);
-            pokeImage("./pokemon-sad.gif")
-        }
-        else {
-            return res.json();
-        }
-    }).then((data) => {
-        if (data) {
-            console.log(data);
-            let pokeImg = data.sprites.front_default;
-            pokeImage(pokeImg);
-            console.log(pokeImg);
-        }
-    });
+    //Realiza promesa de datos en general.
+    let res = await fetch(url);
+    let pokeData = await res.json();
+    console.log(pokeData);
+    //Imagen.
+    let pokeImg = pokeData.sprites.front_default;
+    //Nombre.
+    pokeName=pokeData.name;
+    //Id.
+    let pokeID=pokeData.id;
+    //Tipo. 
+    //let pokeType=data.types[0].type.name; //Inglés
+    //Accede al archivo de tipos para buscar el tipo en un idioma.
+    res = await fetch(pokeData.types[0].type.url);
+    let pokeType = await res.json();
+    //Especie.
+    res = await fetch(pokeData.species.url);
+    let pokeSpecie = await res.json();
+    //Descripcion
+    let pokeDescription = pokeSpecie.flavor_text_entries;
+    const _pokeDescription = pokeDescription.filter(
+        (detail) => detail.language.name === "es");
+    console.log(_pokeDescription[0].flavor_text.replace(/\n/g, " "));
+
+    /*fetch(url)
+        .then((res) => {
+            if (res.status != "200") {
+                console.log(res);
+                pokeImage("./pokemon-sad.gif")
+            }
+            else {
+                return res.json();
+            }
+        })
+        .then((data) => {*/
+    //Inserta la informacion en la pokedex.
+    if (pokeData) {
+        //Imagen.
+        const _pokeImage = document.getElementById("pokeImg");
+        _pokeImage.src = pokeImg;
+        //Nombre
+        const _pokeName=document.getElementById("pokeName");
+        _pokeName.value=pokeName;
+        //Id
+        const _pokeID=document.getElementById("pokeNum");
+        _pokeID.innerHTML=`No. ${pokeID}`;   
+        //Tipo
+        const _pokeType=document.getElementById("pokeType");
+        _pokeType.innerHTML=`<p>Tipo:<br>${pokeType.names[5].name}<p>`;
+        //Especie
+        const _pokeSpecie=document.getElementById("rightScreen");
+        _pokeSpecie.innerHTML=`<p>Especie: ${pokeSpecie.genera[5].genus}<p>`;
+
+        let pokeString=pokeName+". Tipo: "+ pokeType.names[5].name+". Especie: "+pokeSpecie.genera[5].genus+". "+_pokeDescription[0].flavor_text.replace(/\n/g, " ");
+        speak(pokeString);
+        
+    }
+        //});
 }
 
-const pokeImage = (url) => {
-    const pokePhoto = document.getElementById("pokeImg");
-    pokePhoto.src = url;
+function speak(pokeString){
+    //let voices = speechSynthesis.getVoices()[0];
+    let message=new SpeechSynthesisUtterance;
+    message.lang = 'es-US';
+    message.text=pokeString;
+    speechSynthesis.speak(message);
 }
