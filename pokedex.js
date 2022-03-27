be2a7e63449f43a9e4bfee42a880145d9c0596c0
Martin Otamendi <89677633+MartinOtamendiT@users.noreply.github.com>
2022-03-27 05:47:33 -0600

    Add control voice and buttons functionality

--- pokedex.js
+++ pokedex.js
@@ -1,106 +1,155 @@
+const _pokeRightScreen=document.getElementById("rightScreen");
+const _pokeImage = document.getElementById("pokeImg");
+const _pokeID=document.getElementById("pokeNum");
+const _pokeType=document.getElementById("pokeType");
+const _pokeName=document.getElementById("pokeName");
+let pokeDescription;
+const synth = window.speechSynthesis;
+let message=new SpeechSynthesisUtterance;
+message.lang = 'es-US';
+let currentPokemon=25;
+let pokeString="Sin datos para repetir.";
+let pokeIntroduction="Datos no disponibles.";
+let pokeDescriptions=["Datos no disponibles.","Datos no disponibles.","Datos no disponibles.","Datos no disponibles."];
+
 async function getPokemon(){
     //Captura el nombre del pokemon de la entrada.
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
-    //Obtiene archivo del pokemon.
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    //Realiza promesa de datos en general.
-    let res = await fetch(url);
-    let pokeData = await res.json();
-    console.log(pokeData);
-    //Imagen.
-    let pokeImg = pokeData.sprites.front_default;
-    //Nombre.
-    pokeName=pokeData.name;
-    //Id.
-    let pokeID=pokeData.id;
-    //Tipo. 
-    //let pokeType=data.types[0].type.name; //Inglés
-    //Accede al archivo de tipos para buscar el tipo en un idioma.
-    res = await fetch(pokeData.types[0].type.url);
-    let pokeType = await res.json();
-    //Especie.
-    res = await fetch(pokeData.species.url);
-    let pokeSpecie = await res.json();
-    //Descripcion.
-    let pokeDescription = pokeSpecie.flavor_text_entries;
-    const _pokeDescription = pokeDescription.filter(
-        (detail) => detail.language.name === "es");
-    console.log(_pokeDescription[0].flavor_text.replace(/\n/g, " "));
-    //Peso.
-    let pokeWeight=pokeData.weight;
-    //Altura.
-    let pokeHeight=pokeData.height;
-    //Habilidades.
-    let pokeAbilities;
-    res = await fetch(pokeData.abilities[0].ability.url);
-    //console.log(res);
-    pokeAbilities=[await res.json()];
-    res = await fetch(pokeData.abilities[1].ability.url);
-    pokeAbilities.push(await res.json());
-
-    /*fetch(url)
-        .then((res) => {
-            if (res.status != "200") {
-                console.log(res);
-                pokeImage("./pokemon-sad.gif")
-            }
-            else {
-                return res.json();
-            }
-        })
-        .then((data) => {*/
-    //Inserta la informacion en la pokedex.
-    if (pokeData) {
-        //Imagen.
-        const _pokeImage = document.getElementById("pokeImg");
-        _pokeImage.src = pokeImg;
-        //Nombre
-        const _pokeName=document.getElementById("pokeName");
-        _pokeName.value=pokeName;
-        //Id
-        const _pokeID=document.getElementById("pokeNum");
-        _pokeID.innerHTML=`<p>No. ${pokeID}</p>`;   
-        //Tipo
-        const _pokeType=document.getElementById("pokeType");
-        _pokeType.innerHTML=`<p>Tipo:<br>&nbsp&nbsp${pokeType.names[5].name}</p>`;
-        //Especie
-        const _pokeRightScreen=document.getElementById("rightScreen");
-        _pokeRightScreen.innerHTML=`<p>Especie: ${pokeSpecie.genera[5].genus}.<br>
-        Peso: ${pokeWeight}.<br>
-        Altura: ${pokeHeight}.<br>
-        Habilidades:<br>
-        &nbsp&nbsp&nbsp*${pokeAbilities[0].names[5].name}<br>
-        &nbsp&nbsp&nbsp*${pokeAbilities[1].names[5].name}
-        </p>`;
-
-        let pokeString=pokeName+". Tipo: "+ pokeType.names[5].name+". Especie: "+pokeSpecie.genera[5].genus+". "+_pokeDescription[0].flavor_text.replace(/\n/g, " ");
-        speak(pokeString);
-        
+    if (synth.speaking) {
+        synth.cancel();
     }
-        //});
+    //Obtiene archivo del pokemon.
+    try{
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+        //Realiza promesa de datos en general.
+        let res = await fetch(url);
+        let pokeData = await res.json();
+        console.log(pokeData);
+        //Imagen.
+        let pokeImg = pokeData.sprites.other["official-artwork"].front_default;
+        //Nombre.
+        pokeName=pokeData.name;
+        //Id.
+        let pokeID=pokeData.id;
+        currentPokemon=pokeID;
+        //Tipo. 
+        //let pokeType=data.types[0].type.name; //Inglés
+        //Accede al archivo de tipos para buscar el tipo en un idioma.
+        res = await fetch(pokeData.types[0].type.url);
+        let pokeType = await res.json();
+        //Especie.
+        res = await fetch(pokeData.species.url);
+        let pokeSpecie = await res.json();
+        //Descripcion.
+        pokeDescription = pokeSpecie.flavor_text_entries;
+        const _pokeDescription = pokeDescription.filter(
+            (detail) => detail.language.name === "es");
+        pokeDescriptions[0]=_pokeDescription[0].flavor_text.replace(/\n/g, " ");
+        try{
+            pokeDescriptions[1]=_pokeDescription[1].flavor_text.replace(/\n/g, " ");
+            pokeDescriptions[2]=_pokeDescription[2].flavor_text.replace(/\n/g, " ");
+            pokeDescriptions[3]=_pokeDescription[3].flavor_text.replace(/\n/g, " ");
+        }
+        catch(e){
+            console.log("Descripcion no encontrada");
+            pokeDescriptions[2]="";
+            pokeDescriptions[3]="";
+        }
+        //console.log(_pokeDescription[1].flavor_text.replace(/\n/g, " "));
+        //Peso.
+        let pokeWeight=pokeData.weight;
+        //Altura.
+        let pokeHeight=pokeData.height;
+        //Habilidades.
+        let pokeAbilities;
+        let pokeAbilitesLength=pokeData.abilities.length;
+        if(pokeAbilitesLength==1){
+            //Solo tiene una habilidad.
+            res = await fetch(pokeData.abilities[0].ability.url);
+            pokeAbilities=[await res.json()];
+        }
+        else{
+            //Tiene dos habilidades.
+            res = await fetch(pokeData.abilities[0].ability.url);
+            pokeAbilities=[await res.json()];
+            res = await fetch(pokeData.abilities[1].ability.url);
+            pokeAbilities.push(await res.json());
+        }
+
+        //Inserta la informacion en la pokedex.
+        //Imagen.
+        _pokeImage.src = pokeImg;
+        //Nombre.
+        _pokeName.value=pokeName;
+        //Id.
+        _pokeID.innerHTML=`<p>No. ${pokeID}</p>`;   
+        //Tipo.
+        _pokeType.innerHTML=`<p>Tipo:<br>&nbsp&nbsp${pokeType.names[5].name}</p>`;
+        //Especie, peso, altura y habilidades.
+        if(pokeAbilitesLength==1){
+            //Solo tiene una habilidad.
+            _pokeRightScreen.innerHTML=`<p>Especie: ${pokeSpecie.genera[5].genus}.<br>
+                Peso: ${pokeWeight} hm.<br>
+                Altura: ${pokeHeight} dm.<br>
+                Habilidades:<br>
+                &nbsp&nbsp&nbsp*${pokeAbilities[0].names[5].name}</p>`;
+        }
+        else{
+            //Tiene dos habilidades.
+            _pokeRightScreen.innerHTML=`<p>Especie: ${pokeSpecie.genera[5].genus}.<br>
+                Peso: ${pokeWeight} hm.<br>
+                Altura: ${pokeHeight} dm.<br>
+                Habilidades:<br>
+                &nbsp&nbsp&nbsp*${pokeAbilities[0].names[5].name}<br>
+                &nbsp&nbsp&nbsp*${pokeAbilities[1].names[5].name}</p>`;
+        }
+
+        //Se invoca al narrador.
+        pokeIntroduction=pokeName+". Tipo: "+ pokeType.names[5].name+". Especie: "+pokeSpecie.genera[5].genus+". "
+            +pokeDescriptions[0];
+        pokeString=pokeIntroduction;
+        speak(pokeString);  
+    }
+    catch(error){
+        console.log("Error: "+error);
+        _pokeRightScreen.innerHTML=`<p>Ha ocurrido un error.<br><br>
+            Verifique que el nombre del pokémon esté bien escrito o introduzca un número entre 1 y 898.</p>`;
+        _pokeImage.src="Images/squirtle-crying.gif";
+        _pokeID.innerText="";
+        _pokeType.innerText="";
+        _pokeImage.src="Images/squirtle-crying.gif";
+        pokeString="Sin datos para repetir."
+        pokeIntroduction="Datos no disponibles.";
+    }
 }
 
-function speak(pokeString){
-    //let voices = speechSynthesis.getVoices()[0];
-    let message=new SpeechSynthesisUtterance;
-    message.lang = 'es-US';
-    message.text=pokeString;
-    speechSynthesis.speak(message);
+function speak(pokeSpeak){
+    message.text=pokeSpeak;
+    //Detiene al narrador en caso de que este hablando.
+    if (synth.speaking) {
+        synth.cancel();
+    }
+    synth.speak(message);
     message.onstart = () => {
         onBlinkBlueLight();
     };
     message.onend = () => {
         offBlinkBlueLight();
-    };
-         
+    };        
 }
 
+//Detiene al narrador al recargar la página.
+document.addEventListener("DOMContentLoaded", (e) => {
+    synth.cancel();
+});
+//Hace parpadear al led azul.
 function onBlinkBlueLight(){
     const blueLight=document.getElementById("blueLight");
     blueLight.classList.add("blinkBlueLight");
 }
+//El led azul deja de parpadear.
 function offBlinkBlueLight(){
     const blueLight=document.getElementById("blueLight");
     blueLight.classList.remove("blinkBlueLight");
@@ -108,7 +157,30 @@ function offBlinkBlueLight(){
 
 //Elige un pokemon de entre 898 al azar para buscar.
 function pokeRandom(){
-    const _pokeName=document.getElementById("pokeName");
     _pokeName.value=Math.floor(Math.random() * 898)+ 1;
     getPokemon();
+}
+
+//Busca al pokemon siguiente.
+function nextPokemon(){
+    _pokeName.value=currentPokemon+1;
+    getPokemon();
+}
+//Busca al anterior Pokemon.
+function previousPokemon(){
+    _pokeName.value=currentPokemon-1;
+    getPokemon();
+}
+//Detiene al narrdor.
+function stopSpeaking(){
+    synth.cancel();
+}
+//Repite lo último dicho por el narrador.
+function repeatSpeaking(){
+    speak(pokeString);
+}
+//Reproduce una de las descripciones del pokemon.
+function playPokeDescription(index){
+        pokeString=pokeDescriptions[index];
+        speak(pokeString);
 }
